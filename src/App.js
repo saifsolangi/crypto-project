9242501311efcc6cb6b2c59c2ca4834932f9f1e9
Saif Ali <saifsolangi2@gmail.com>
2023-08-23 15:06:33 +0500

    finaly

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
 
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en'
 
-  useEffect(() => {
-    axios.get(url).then((response) => {
+  useEffect(async () => {
+    await axios.get(url).then((response) => {
       setCoins(response.data)
       // console.log(response.data)
     }).catch((err) => {
